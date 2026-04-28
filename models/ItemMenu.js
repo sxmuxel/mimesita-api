b872c077130177b609159d6d434d49048e38141f
Sebastian <Sebastian1234@gmail.com>
2026-04-28 17:11:20 -0500

    Se corrige error de ortografía

--- models/ItemMenu.js
+++ models/ItemMenu.js
@@ -5,7 +5,7 @@ const itemMenuSchema = new mongoose.Schema(
     restaurante: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Restaurante',
-      required: [true, 'El restaurante es obligatorio'],
+      required: [true, 'El restaurante es muy obligatorio'],
     },
     nombre: {
       type: String,
